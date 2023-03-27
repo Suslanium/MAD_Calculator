aedd9ce6f3117d4636acb091ab939a6ca3b6fe16
Suslanium <zaharov22m@gmail.com>
2023-03-28 02:20:47 +0700

    -Refactor #2

--- app/src/main/java/com/suslanium/calculator/model/CalculationResult.kt
+++ app/src/main/java/com/suslanium/calculator/model/CalculationResult.kt
@@ -2,8 +2,8 @@ package com.suslanium.calculator.model
 
 sealed interface CalculationResult {
     data class Success(val output: String) : CalculationResult
-    
+
     object Failure : CalculationResult
-    
+
     object Nothing : CalculationResult
 }
